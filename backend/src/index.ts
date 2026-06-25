5cd378aad6b4e633bb33910256c805b74d9b2c45
Aybike Gucuk <aybike.gck@gmail.com>
2026-06-25 17:17:22 +0300

    güncel3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import { pool } from "./database";
 import cors from "cors";
+
 const app = express();
 const PORT = 5001;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("🏨 Hotel Backend Running!");
 });
 
+/* ---------------- RESERVATION ---------------- */
 app.post("/reservations", async (req, res) => {
     try {
         const { name, email, phone, check_in, check_out, room_type, guests } = req.body;
@@ -24,6 +25,25 @@ app.post("/reservations", async (req, res) => {
         );
 
         res.status(201).json({ message: "Reservation created successfully" });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+/* ---------------- CONTACT ---------------- */
+app.post("/contact", async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+
+        await pool.query(
+            "INSERT INTO contact_messages (name, email, message) VALUES ($1,$2,$3)",
+            [name, email, message]
+        );
+
+        res.status(201).json({ message: "Message sent successfully" });
+
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Server error" });
